ad1e3e946c40623adf7f75e08d876269a6043d04
lucas <lucasgheno600@gmail.com>
2024-08-26 21:56:20 -0300

    feat: making landin-page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import logoSvg from "../assets/logo.svg";
 import Link from "next/link";
 import runSvg from "../assets/run.svg";
 import plansSvg from "../assets/plans.svg";
+
 export default function Home() {
   return (
     <main>
@@ -11,7 +12,7 @@ export default function Home() {
         id="classe"
         className="mx-auto flex p-4 flex-col min-h-screen w-full max-w-screen-xl"
       >
-        <nav className="w-full mt-8 h-5 flex items-center justify-between">
+        <nav className="w-full mt-8 flex flex-col md:flex-row items-center justify-between">
           <Link href="/">
             <Image
               className="h-12 w-12"
@@ -20,7 +21,7 @@ export default function Home() {
             />
           </Link>
 
-          <div className="flex gap-8 items-center">
+          <div className="links flex gap-4 items-center mt-4 md:mt-0">
             <Link
               href="#classe"
               className="font-bold text-stone-700 hover:text-blue-700 transition-colors"
@@ -35,16 +36,16 @@ export default function Home() {
             </Link>
             <Link
               href="#classe"
-              className="font-bold px-5 py-3 text-white bg-blue-700 hover:bg-blue-800 transition-colors rounded-full"
+              className="cta-button font-bold px-5 py-3 text-white bg-blue-700 hover:bg-blue-800 transition-colors rounded-full "
             >
               Começar
             </Link>
           </div>
         </nav>
 
-        <div className="flex mt-20 items-center justify-between w-full">
-          <div className="items-center justify-between">
-            <h1 className="text-left text-7xl leading-15 font-black text-stone-800">
+        <div className="flex mt-20 flex-col md:flex-row items-center justify-between w-full">
+          <div className="items-center justify-between text-center md:text-left">
+            <h1 className="text-4xl md:text-7xl leading-tight md:leading-15 font-black text-stone-800">
               Treine na academia dos seus{" "}
               <span className="text-blue-700">sonhos</span>.
             </h1>
@@ -53,20 +54,20 @@ export default function Home() {
             </p>
           </div>
           <Image
-            className="w-full mt-10 max-w-2xl"
+            className="w-full mt-10 max-w-full md:max-w-2xl"
             src={gymSvg}
             alt="Ilustração de uma academia."
           />
         </div>
 
-        <div className="mt-20 flex items-center justify-between">
+        <div className="mt-20 flex flex-col md:flex-row items-center justify-between">
           <Image
-            className="w-full max-w-xl"
+            className="w-full max-w-full md:max-w-xl"
             src={runSvg}
             alt="Ilustração de uma pessoa correndo."
           />
-          <div className="ml-10">
-            <h2 className="text-4xl font-bold text-stone-800">
+          <div className="mt-10 md:ml-10 text-center md:text-left">
+            <h2 className="text-2xl md:text-4xl font-bold text-stone-800">
               Corrida é o seu forte?
             </h2>
             <p className="text-lg text-stone-700 mt-4">
@@ -77,17 +78,19 @@ export default function Home() {
             </p>
           </div>
         </div>
+
         <div className="mt-20 flex justify-center">
           <Image
-            className="w-full max-w-2xl"
+            className="w-full max-w-full md:max-w-2xl"
             src={plansSvg}
             alt="Ilustração dos planos da academia."
           />
         </div>
+
         <footer className="mt-20 p-8 bg-stone-100 w-full">
           <div className="flex flex-col md:flex-row justify-between space-y-8 md:space-y-0 md:space-x-8">
             <div className="md:w-1/3">
-              <h3 className="text-2xl font-bold text-stone-800">Sobre </h3>
+              <h3 className="text-2xl font-bold text-stone-800">Sobre</h3>
               <p className="text-stone-700 mt-4">
                 Projeto de uma landing-page fictícia para uma academia, feita
                 somente para fins de estudo e prática de desenvolvimento web.
