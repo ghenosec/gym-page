467da7aa977e3a25030337cd41d72c636dae7c0b
Ghenoo <lucasgheno600@gmail.com>
2024-10-14 14:43:47 -0300

    add process.env in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+  env: {
+    EMAIL_HOST: process.env.EMAIL_HOST,
+    EMAIL_PORT: process.env.EMAIL_PORT,
+    EMAIL_SECURE: process.env.EMAIL_SECURE,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    RECEIVER_EMAIL: process.env.RECEIVER_EMAIL,
+  },
+};
 
 export default nextConfig;
